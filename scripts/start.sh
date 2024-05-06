0b2390a14c384d9f8fa684de1e741274c8fc2490
Hayden Dekker <hayden.d@hotmail.com>
2024-05-06 13:30:55 +1000

    Docker build via appspec
    appspec builds docker image.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,26 @@
 echo "start"
-sudo systemctl start website-image-scheduler.service
+
+servicesName=$HOSTNAME
+
+if [[ -z "$servicesName" ]]; then
+  echo "No Environment Services Name set for ENV Variable ENV_SERVICES_NAME"
+  exit 1
+else
+  echo "Environment Services Folder Name is: $servicesName"
+fi
+
+composeFilePath="/etc/envman/orchestration/$servicesName/"
+
+echo "Checking file exists at $composeFilePath"
+
+if [[ -f "$composeFilePath" ]]; then
+  echo "Docker comopse found for $servicesName."
+else
+  echo "docker-compose.yml found for $servicesName."
+fi
+
+composeDir="${composeFilePath%/*}"
+
+cd composeDir
+
+docker-compose up
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
 echo "validate"
+
+container_name="website-image-scheduler"
+matching_containers=$(docker-compose ps -q --filter="name=${container_name}" | grep -E "^/${container_name}$")
+
+if [[ ! -z "$matching_containers" ]]; then
+  echo "Found containers with name '${container_name}':"
+  docker-compose ps | grep "${container_name}"
+else
+  echo "No containers found with name '${container_name}'"
+  exit 1
+fi
