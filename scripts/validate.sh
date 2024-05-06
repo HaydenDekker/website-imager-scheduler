e4a995a6b9bd65a6a9cbcb0f7da75f7f60ddd4ce
Hayden Dekker <hayden.d@hotmail.com>
2024-05-06 17:16:26 +1000

    Bugfix - syntax

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -6,7 +6,7 @@ echo "Checking file exists at $scriptValidate"
 
 if [[ -f "$scriptValidate" ]]; then
   echo "Env validate script found."
-  . scriptValidate website-image-scheduler
+  . $scriptValidate website-image-scheduler
 else
   echo "Env start script not found on server. Server should be a managed env."
 fi
