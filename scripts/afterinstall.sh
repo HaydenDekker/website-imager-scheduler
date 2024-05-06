8294deea891e5ba7c5275633d99a1ca214033cfe
Hayden Dekker <hayden.d@hotmail.com>
2024-05-06 15:46:14 +1000

    output jar count for failure

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,5 +1,7 @@
 echo "after install"
 
+pwd 
+
 jar_count=$(find . -type f -name "*.jar" | wc -l)
 
 if [[ $jar_count -eq 1 ]]; then
@@ -8,7 +10,7 @@ else
   if [[ $jar_count -eq 0 ]]; then
     echo "No jar files found."
   else
-    echo "More than one jar file found."
+    echo "More than one jar file found $jar_count."
   fi
   exit 1
 fi
